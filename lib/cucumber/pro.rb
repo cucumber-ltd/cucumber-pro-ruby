5f58e5dbd0213316df422f11cae4616102d88c9c
Matt Wynne <matt@mattwynne.net>
2013-10-05 23:48:54 +0100

    Grab the basics from the Cucumber formatter API

--- lib/cucumber/pro.rb
+++ lib/cucumber/pro.rb
@@ -1,4 +1,18 @@
 module Cucumber
   module Pro
+    def self.new(*args)
+      Formatter.new(*args)
+    end
+
+    class Formatter
+      def initialize(runtime, io, options)
+      end
+
+      def before_step_result(*args)
+        raise "Not ready to deal with new API" if args.length == 1
+        keyword, step_match, multiline_arg, status, exception, source_indent, background, file_colon_line = *args
+        p [file_colon_line, status]
+      end
+    end
   end
 end
