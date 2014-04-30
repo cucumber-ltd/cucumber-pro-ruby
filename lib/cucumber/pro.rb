184f0cae24c50e5e3f3b225ddabb51d77d08c40a
Matt Wynne <matt@mattwynne.net>
2014-04-30 08:19:54 +0100

    Refactor, extracting methods to send data to session

--- lib/cucumber/pro.rb
+++ lib/cucumber/pro.rb
@@ -11,41 +11,25 @@ module Cucumber
     class Formatter
       def initialize(runtime, io, options)
         logger = Logger.new(ENV['cucumber_pro_log_path'] || STDOUT)
-        scm = Scm::Repo.find
         @session = WebSocketSession.new('localhost', 5000, logger)
-        @session.send({
-          repo_url: scm.remote,
-          branch: scm.branch,
-          rev: scm.rev,
-          group: get_run_id
-        })
+        send_header
       end
 
       def before_feature(feature)
-        @path = feature.file
+        @path = feature.file # we need this because table_row doens't have a file_colon_line
       end
 
       def before_step_result(*args)
         keyword, step_match, multiline_arg, status, exception, source_indent, background, file_colon_line = *args
         path, line = *file_colon_line.split(':')
-        @session.send({
-          path: path,
-          location: line.to_i,
-          mime_type: 'application/vnd.cucumber.test-step-result+json',
-          body: { status: status }
-        })
+        send_step_result(path, line, status)
       end
 
       def after_feature_element(feature_element)
         return if Cucumber::Ast::ScenarioOutline === feature_element
         scenario = feature_element
         path, line = *scenario.file_colon_line.split(':')
-        @session.send({
-          path: path,
-          location: line.to_i,
-          mime_type: 'application/vnd.cucumber.test-case-result+json',
-          body: { status: scenario.status }
-        })
+        send_test_case_result(path, line, scenario.status)
       end
 
       def before_examples(*args)
@@ -59,15 +43,11 @@ module Cucumber
 
       def after_table_row(table_row)
         return unless @in_examples and Cucumber::Ast::OutlineTable::ExampleRow === table_row
-        unless @header_row
-          @session.send({
-            path: @path,
-            location: table_row.line,
-            mime_type: 'application/vnd.cucumber.test-case-result+json',
-            body: { status: table_row.status }
-          })
+        if @header_row
+          @header_row = false
+          return
         end
-        @header_row = false if @header_row
+        send_test_case_result(@path, table_row.line, table_row.status)
       end
 
       def after_features(*args)
@@ -76,9 +56,38 @@ module Cucumber
 
       private
 
+      def send_header
+        scm = Scm::Repo.find
+        @session.send({
+          repo_url: scm.remote,
+          branch: scm.branch,
+          rev: scm.rev,
+          group: get_run_id
+        })
+      end
+
+      def send_step_result(path, line, status)
+        @session.send({
+          path: path,
+          location: line.to_i,
+          mime_type: 'application/vnd.cucumber.test-step-result+json',
+          body: { status: status }
+        })
+      end
+
+      def send_test_case_result(path, line, status)
+        @session.send({
+          path: path,
+          location: line.to_i,
+          mime_type: 'application/vnd.cucumber.test-case-result+json',
+          body: { status: status }
+        })
+      end
+
       def get_run_id
         Time.now.to_i
       end
+
     end
 
   end
