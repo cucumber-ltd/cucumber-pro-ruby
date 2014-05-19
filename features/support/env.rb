6acfeeec68b7dbf787af1182bce1174244b6c30e
Matt Wynne <matt@mattwynne.net>
2014-05-19 19:58:29 +0100

    Force the config to be loaded so we can clean up the env vars

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,3 +1,8 @@
 $logger = Logger.new(ENV['cucumber_pro_log_path'] || STDOUT)
 $logger.debug '--- starting tests ---'
+
+# You don't normally need this in your test suite, but we need to make sure this environment
+# variable has been read so we can delete it, because otherwise it will interfere with other
+# parts of our test suite.
+require 'cucumber/pro'
 ENV.delete('CUCUMBER_PRO_TOKEN')
